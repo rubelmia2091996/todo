e22ae6cc4affc8fc95a17d60f5a45e89f8241428
Rubel Mia <85490076+rubelmia2091996@users.noreply.github.com>
2025-02-06 12:53:40 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/todos');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
